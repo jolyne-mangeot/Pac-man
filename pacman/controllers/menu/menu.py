f3754c6eae0074f15abf8b4e04a029fc9405605a
jolyne-mangeot <jolyne.mangeot@gmail.com>
2026-07-31 17:53:15 +0200

    docs[Menu class]: add docstrings for all Menu class methods

--- pacman/controllers/menu/menu.py
+++ pacman/controllers/menu/menu.py
@@ -3,7 +3,7 @@ import pygame as pg
 from typing import cast, Any
 
 from pacman.models import KeyConfig
-from .utils import Option, ActivateOption, ToggleOption
+from .utils import Option
 
 
 def key_unicode_to_action(key_config: KeyConfig, input: str) -> str:
@@ -21,10 +21,76 @@ def key_unicode_to_action(key_config: KeyConfig, input: str) -> str:
 
 
 class Menu:
-    """A class that represents an options menu."""
+    """Class Menu
+
+    Made to list options of different types on the pygame window, and handling
+    the user's input accordingly.
+
+    Takes a list of Option subclasses objects which text's attribute to
+    display in multiple available arrangements and to pass down the user's
+    input to, supporting the use of different fonts and colors for the states
+    of the options: deselected, selected and picked. Images can also be
+    displayed next to the options if given any.
+
+    ### Attributes:
+    <u>Parameters:</u>
+    - screen: pg.Surface => the screen onto which display the
+    options
+    - from_left: int => x coordinates of the screen to place the
+    display of options, corresponds to the center of the options
+    - from_top: int => y coordinates of the screen to place the
+    options, corresponds to the center of the upmost option.
+    - spacer: int => multiplied factor separating the options
+    - options: list[Option] => list containing all Option instances
+    to manage the Menu
+    - deselected_color: pg.Color => color for unselected options
+    - selected_color: pg.Color => color for selected options
+    - picked_color: pg.Color => color for picked options
+    - deselect_ft: pg.font.Font => font for unselected options
+    - selected_ft: pg.font.Font => font for selected options
+    - picked_ft: pg.font.Font => font for picked options
+    - images: list[pg.Surface] => list of pre-scaled images to
+    display next to each options.
+
+    <u>Self attributed:</u>
+    - select_index: int => index of the selected option, like a cursor
+    - picked_index: int => index of the picked option, which will receive
+    further inputs
+    - last_picked: int => index of the last picked option to update its render
+    after it's been interacted with
+    - rendered: dict[str, list[tuple[pg.Surface, pg.Rect]]] => options rendered
+    as pygame Surfaces, saving their font, size, style and color for later
+    display.
+
+    ### Methods:
+    <u>Option rendering:</u>
+    - pre_render_all_options => Reset self.render and append each of its lists
+    with pygame Surfaces and Rects of each option.
+    - pre_render_option => Override a single option's renders in each render
+    lists to update its visual.
+    - pre_render => Render the visuals for the Option's str cast using the
+    associated fonts and colors to return a tuple of visuals for the
+    deselected, selected and picked styles.
+
+    <u>Event handling:</u>
+    - get_event => get a pygame Event object to manage options, either changing
+    the selected one, picking one or deactivating one. Handles different
+    visual configuration like vertical or horizontal menues.
+    - get_event_vertical => handles action input to accord to a vertical
+    arrangement of the options with "up_key" and "down_key" inputs
+    - get_event_horizontal => handles action input to accord to a horizontal
+    arrangement of the options
+    - get_event_chart => handles action input to accord to a chart-like
+    arrangement of the options (like table cells)
+    - move_cursor => calls change_selected_option until the currently selected
+    option has its attribute "selectable" on True
+    - change_selected_option => Apply the factor in argument to the
+    select_index, checking for range limits
+    """
     def __init__(
             self, screen: pg.Surface, from_left: int = -1, from_top: int = -1,
             spacer: int = 50, options: list[Option] = [],
+            loop_cursor: bool = True,
             deselected_color: pg.Color = pg.Color(0, 0, 0),
             selected_color: pg.Color = pg.Color(0, 0, 0),
             picked_color: pg.Color = pg.Color(255, 0, 0),
@@ -41,6 +107,7 @@ class Menu:
             from_top if from_top != -1 else int(screen.get_height() / 2))
         self.spacer: int = spacer
         self.options: list[Option] = options
+        self.loop_cursor: bool =loop_cursor
         self.deselected_color: pg.Color = deselected_color
         self.selected_color: pg.Color = selected_color
         self.picked_color: pg.Color = picked_color
@@ -55,6 +122,11 @@ class Menu:
         self.pre_render_all_options()
 
     def pre_render_all_options(self) -> None:
+        """To be called when all options' visuals need to be updated.
+
+        Reset the rendered dict to empty lists, then loop through every option
+        to render their visuals to later be displayed on screen.
+        """
         self.rendered = {"deselected": [], "selected": [], "picked": []}
         des: tuple[pg.Surface, pg.Rect]
         sel: tuple[pg.Surface, pg.Rect]
@@ -66,6 +138,10 @@ class Menu:
             self.rendered["picked"].append(pik)
 
     def pre_render_option(self, index: int = -1) -> None:
+        """Call pre_render on the option which index is given as argument,
+        updating each list in the rendered dict with the returned tuple of
+        renders.
+        """
         des: tuple[pg.Surface, pg.Rect]
         sel: tuple[pg.Surface, pg.Rect]
         pik: tuple[pg.Surface, pg.Rect]
@@ -81,8 +157,10 @@ class Menu:
         self.rendered["picked"][index] = pik
 
     def pre_render(self, option: Option) -> tuple[tuple[pg.Surface, pg.Rect]]:
-        """re-renders the options in three states: picked, deselected,
-        and selected.
+        """Pre-renders the option given as argument in three states: picked,
+        deselected and selected, each with their associated fonts and colors,
+        then returns a tuple of each state, as a tuple of pygame Surface and
+        Rect.
         """
         render: str = str(option) if option is not None else ""
         deselect_render = self.deselect_ft.render(
@@ -220,7 +298,28 @@ class Menu:
                 self.screen.blit(select_render[0], select_render[1])
 
     def get_event(self, key_config: KeyConfig,
-                  event: pg.event.Event, disposition: str) -> Any:
+                  event: pg.event.Event, arrangement: str) -> Any:
+        """Takes a key_config, a pygame event and an arrangement as arguments.
+
+        Using the currently selected option, which can also be the picked
+        option, handles the way the user can interact with it.
+
+        The input is first converted to a string, either as a text_input if
+        the option needs it, or a named key, then translated into an action
+        key based on the key_config. When done, if the confirm_key or
+        return_key was pressed, either activate or deactivate the option by
+        calling its dedicated methods, and reinitialize the picked_index to -1.
+
+        Otherwise, pass down the event strings to the picked option by
+        calling its input_event method, and recovering its output. If it's
+        "action_done", deactivate it, and otherwise return it.
+
+        Finally, if no option was picked and the input in different from
+        confirm and return, the method to move the cursor based on the given
+        arrangement is called.
+
+        Returns Any as options can also do.
+        """
         curr_option: Option = self.options[self.select_index]
         input: str = ""
         if event.type == pg.KEYDOWN and (curr_option.using_text_input is False
@@ -235,7 +334,7 @@ class Menu:
         if action_key == "confirm_key":
             if self.picked_index == -1:
                 self.picked_index = self.select_index
-                self.last_index = -1
+                self.last_picked = -1
                 self.options[self.picked_index].activate()
                 action_key = "activate"
             else:
@@ -251,18 +350,19 @@ class Menu:
                 self.options[self.picked_index].deactivate()
                 self.last_picked = self.picked_index
                 self.picked_index = -1
-                return ""
+                return
             else:
                 return output
         {
             "horizontal": self.get_event_horizontal,
             "vertical": self.get_event_vertical,
-            "chart": self.get_event_chart}[disposition](action_key)
-        return ""
+            "chart": self.get_event_chart}[arrangement](action_key)
 
     def get_event_vertical(self, key_input: str) -> None:
         """Processes vertical movement (up and down) in the menu based on key
-        events.
+        events. The key_input given as argument, if in "up_key" and "down_key",
+        is used to move the selection cursor along the options, using the
+        move_cursor method with a set factor.
         """
         if key_input == "up_key":
             self.move_cursor(-1)
@@ -271,7 +371,9 @@ class Menu:
 
     def get_event_horizontal(self, key_input: str) -> None:
         """Processes horizontal movement (left and right) in the menu based on
-        key events.
+        key events. The key_input given as argument, if in "left_key" and
+        "right_key", is used to move the selection cursor along the options,
+        using the move_cursor method with a set factor.
         """
         if key_input == "left_key":
             self.move_cursor(-1)
@@ -279,6 +381,11 @@ class Menu:
             self.move_cursor(1)
 
     def get_event_chart(self, key_input: str) -> None:
+        """Processes movements in all directions in the menu based on key
+        events. The key_input given as argument, is used to move the selection
+        cursor along the options, using the move_cursor method with a set
+        factor. Calls get_event_horizontal to handle left right movements.
+        """
         if key_input == "up_key":
             self.move_cursor(-2)
         elif key_input == "down_key":
@@ -291,14 +398,28 @@ class Menu:
             self.get_event_horizontal(key_input)
 
     def move_cursor(self, operant: int) -> None:
+        """Updates the selected index with the change_selected_option method
+        until the currently held option can be selected, by its selectable
+        attribute.
+        """
         self.change_selected_option(operant)
         while self.options[self.select_index].selectable is False:
             self.change_selected_option(operant)
 
     def change_selected_option(self, operant: int) -> None:
+        """Modifies the select_index with the operant given as argument, and
+        compares it with the range of the number of options in the list.
+        If any limit of this range is reached, if the loop_cursor flag is
+        True, the select_index cycles back to minimum or maximum depending on
+        the case, otherwise sitting at the first reached limit.
+        """
         self.select_index += operant
         max_indicator = len(self.rendered["deselected"]) - 1
         if self.select_index < 0:
-            self.select_index = max_indicator
+            if self.loop_cursor is True:
+                self.select_index = max_indicator + self.select_index
         elif self.select_index > max_indicator:
-            self.select_index = 0
+            if self.loop_cursor is True:
+                self.select_index = self.select_index % max_indicator
+            else:
+                self.select_index = max_indicator
