c039aa1e53bb745efa1babf52a2dfef1a5820f73
Belladone-Bzz <belladone.gaming@gmail.com>
2026-08-06 12:22:26 +0200

    update[.gitignore]: remove **/test.py because I AM DUMB

---
+++ pacman/models/entity/test.py
@@ -0,0 +1,9 @@
+"""File: /pacman/models/entity/test.py
+
+Date: 2026-08-05
+#### Description: Tester for entity module. Tests are done on a maze generated
+thanks to mazegenerator.py and displayed in the terminal with ASCII char.
+This test check:
+ - Position of pacman
+ - Position of ghosts
+"""
